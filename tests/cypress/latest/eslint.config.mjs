a9c515d446ee3fdae61ca827941eff8d9f5359fe
Tomas Hehejik <thehejik@suse.com>
2025-09-30 18:00:09 +0200

    Enable prettier plugin for eslint and create lint-fix target for npm

--- tests/cypress/latest/eslint.config.mjs
+++ tests/cypress/latest/eslint.config.mjs
@@ -1,26 +1,40 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 import pluginCypress from 'eslint-plugin-cypress';
+import pluginPrettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
-export default[
-   pluginCypress.configs.recommended,
+export default [
+  pluginCypress.configs.recommended,
 
-   {
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
-        "cypress": pluginCypress,
+      '@typescript-eslint': typescriptEslint,
+      cypress: pluginCypress,
+      prettier: pluginPrettier,
     },
 
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 6,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2021,
+      sourceType: 'module',
     },
 
     rules: {
-        "eol-last": ["error", "always"],
-        "cypress/no-unnecessary-waiting": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "cypress/unsafe-to-chain-command": "off",
+      'prettier/prettier': [
+        'warn',
+        {
+          semi: true,
+          singleQuote: true,
+          tabWidth: 2,
+          useTabs: false,
+        },
+      ],
+      indent: ['warn', 2],
+      'eol-last': ['error', 'always'],
+      'cypress/no-unnecessary-waiting': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'cypress/unsafe-to-chain-command': 'off',
     },
-}];
+  },
+];
